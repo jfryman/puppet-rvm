87a1f35f05a036dd6bf9268022ef8de3e70d3833
James Fryman <james@frymanet.com>
2012-01-16 11:38:13 -0600

    adding absolute path to rvm default version

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,8 +1,26 @@
 class rvm::config {
+  File {
+    owner => 'root',
+    group => 'root',
+    mode  => '0755',
+  }
+
   file { '/etc/gemrc':
-    owner  => 'root',
-    group  => 'root',
     mode   => '0444',
     source => 'puppet:///modules/rvm/etc/gemrc',
+  }
+
+  file { '/etc/profile.d/rvm.sh':
+    source => 'puppet:///modules/rvm/etc/profile.d/rvm.sh',
+  }
+
+  exec { 'rvm-cleanup':
+    command     => '/usr/local/rvm/bin/rvm cleanup sources',
+    refreshonly => 'true',
+  }
+
+  exec { 'upgrade-rvm':
+    command    => '/usr/local/rvm/bin/rvm update ; /usr/local/rvm/bin/rvm reload',
+    refreshony => 'true',
   }
 }
--- manifests/define/version.pp
+++ manifests/define/version.pp
@@ -3,22 +3,22 @@ define rvm::define::version (
   $system = 'false'
 ) {
   include rvm
-  
+
   ## Set sensible defaults for Exec resource
   Exec {
     path    => '/usr/local/rvm/bin:/bin:/sbin:/usr/bin:/usr/sbin',
   }
-  
+
   # Local Parameters
   $rvm_ruby = '/usr/local/rvm/rubies'
-  
+
   # Install or uninstall RVM Ruby Version
   if $ensure == 'present' {
     exec { "install-ruby-${name}":
       command => "rvm install ${name}",
       creates => "${rvm_ruby}/${name}",
       require => Class['rvm'],
-    }  
+    }
   } elsif $ensure == 'absent' {
     exec { "uninstall-ruby-${name}":
       command => "rvm uninstall ${name}",
@@ -26,15 +26,16 @@ define rvm::define::version (
       require => Class['rvm'],
     }
   }
-  
+
   # Establish Default System Ruby.
   # Only create one instance to prevent multiple ruby
   # versions from attempting to be system default.
   if ($system == 'true') and ($ensure != 'absent') {
-    exec { 'set-default-ruby-rvm':
-      command => "rvm --default use ${name}",
-      unless  => "rvm current | grep ${name}",
+    exec { "set-default-ruby-rvm-to-${name}":
+      command => "/usr/local/rvm/bin/rvm --default ${name}",
+      unless  => "rvm list | grep '=> ${name}'",
       require => [Class['rvm'], Exec["install-ruby-${name}"]],
+      notify  => Exec['rvm-cleanup'],
     }
   }
 }
