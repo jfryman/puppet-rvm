f13710104d1ae75766273ee4e92bcca42683d95f
Sebastian Schulze <github.com@bascht.com>
2012-02-17 16:16:40 +0100

    - condition looks cleaner with a regex ;-)

--- manifests/packages/ubuntu.pp
+++ manifests/packages/ubuntu.pp
@@ -1,7 +1,7 @@
 class rvm::packages::ubuntu {
 
   $package_list = $lsbdistcodename ? {
-    'hardy', 'intrepid', 'jaunty', 'karmic', 'lucid' => ['build-essential',
+    /hardy|intrepid|jaunty|karmic|lucid/ => ['build-essential',
                 'bash', 'gawk', 'sed', 'grep', 'gzip', 'bzip2',
                 'zlib1g-dev', 'libssl-dev', 'libreadline-dev'],
     default => ['build-essential', 'bash', 'gawk', 'sed', 'grep',
