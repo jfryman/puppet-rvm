3360092a3bb8aa4e6fd729ba575cd55fa7b52a85
Ramón Cahenzli <ramon.cahenzli@gmail.com>
2012-09-05 16:26:54 +0300

    You cannot do dependencies like that (it clashes with other modules)

--- manifests/packages/debian.pp
+++ manifests/packages/debian.pp
@@ -1,14 +1,15 @@
 class rvm::packages::debian {
-  $package_list = ['build-essential', 'curl', 'bash', 'gawk', 'sed', 'grep',
-                   'gzip', 'bzip2', 'zlib1g-dev', 'libssl-dev', 
-                   'libreadline5-dev']
-                   
-  # Virtualize Package list to prevent conflicts
-  @package { $package_list:
-    ensure => 'present',
-    tag    => 'rvm-packages',
-  }
-  
-  # Realize packages list. 
-  Package<| tag == 'rvm-packages' |>
+ 
+  if ! defined(Package['build-essential'])      { package { 'build-essential':      ensure => installed } }
+  if ! defined(Package['curl'])                 { package { 'curl':                 ensure => installed } }
+  if ! defined(Package['bash'])                 { package { 'bash':                 ensure => installed } }
+  if ! defined(Package['gawk'])                 { package { 'gawk':                 ensure => installed } }
+  if ! defined(Package['sed'])                  { package { 'sed':                  ensure => installed } }
+  if ! defined(Package['grep'])                 { package { 'grep':                 ensure => installed } }
+  if ! defined(Package['gzip'])                 { package { 'gzip':                 ensure => installed } }
+  if ! defined(Package['bzip2'])                { package { 'bzip2':                ensure => installed } }
+  if ! defined(Package['zlib1g-dev'])           { package { 'zlib1g-dev':           ensure => installed } }
+  if ! defined(Package['libssl-dev'])           { package { 'libssl-dev':           ensure => installed } }
+  if ! defined(Package['libreadline5-dev'])     { package { 'libreadline5-dev':     ensure => installed } }
+
 }
